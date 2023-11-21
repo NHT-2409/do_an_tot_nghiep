b44d9ac4c1547311dcbb96145aac69e747d0d816
Hoang Tai <hoangtai.ht8@gmail.com>
2023-11-22 00:41:15 +0700

    check-links-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { DetailComponent } from './detail/detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    DetailComponent,
+    ContactComponent,
+    CheckoutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
